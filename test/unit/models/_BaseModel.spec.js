0cc80b32b9e6e30f267850c7c5be57cf57353e9d
Nick Hoffman <njhoffman1982@gmail.com>
2017-05-25 11:32:51 -0500

    unit tests, refactoring

---
+++ test/unit/models/_BaseModel.spec.js
@@ -0,0 +1,29 @@
+const _BaseModel = require('../../../lib/models/_BaseModel');
+
+describe('Models', () => {
+  describe('_BaseModel', () => {
+    describe('Static Methods', () => {
+      describe('get db',  () => {
+        it('should not suck a dick', () => {
+          expect(true).to.be.true;
+        });
+      });
+      describe('get conn', () => { });
+      describe('get count', () => { });
+      describe('get all', () => { });
+      describe('findById', () => { });
+      describe('modelById', () => { });
+      describe('findByField', () => { });
+      describe('modelByField', () => { });
+      describe('generate', () => { });
+      describe('empty', () => { });
+      describe('reset', () => { });
+    });
+    describe('Instance Methods', () => {
+      describe('constructor', () => { });
+      describe('delete', () => { });
+      describe('update', () => { });
+      describe('add', () => { });
+    });
+  });
+});
