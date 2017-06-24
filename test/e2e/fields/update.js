c4c076130a274c915cdbba5c64d94625321bd3eb
Nick Hoffman <njhoffman1982@gmail.com>
2017-06-23 21:11:04 -0500

    standard model tests, remove iojs from travis build

--- test/e2e/fields/update.js
+++ test/e2e/fields/update.js
@@ -9,6 +9,7 @@ module.exports = function() {
       return setupServer()
         .then(_app => (app = _app));
     });
+
     it('Should return 401 if not authenticated', (done) => {
       request(app)
         .post('/fields/update')
@@ -17,5 +18,49 @@ module.exports = function() {
           done();
         });
     });
+
+    it('Should update existing field with validated fields', (done) => {
+      const data = { id: 0, label: 'New Label', tabName: 'New Tab Name' };
+      login(app)
+        .then(headers => {
+          request(app)
+            .post('/fields/update')
+            .set(headers)
+            .send(data)
+            .then(res => {
+              expect(res.statusCode).to.equal(200);
+              expect(res.body.data) .to.be.an('object').that.contains(data);
+              return request(app).get('/admin/list/fields')
+            }).then(res => {
+              expect(res.statusCode).to.equal(200);
+              expect(res.body[0]).to.be.an('object').that.contains(data);
+              done();
+            }).catch(done);
+        });
+    });
+
+    it('Should ignore fields not in table schema', (done) => {
+      const data = { _badFieldName: 'shouldnt exist', id: 0 };
+      login(app)
+        .then(headers => {
+          request(app)
+            .post('/fields/update')
+            .set(headers)
+            .send(data)
+            .then(res => {
+              expect(res.statusCode).to.equal(200);
+              expect(res.body.data).to.be.an('object').that.contains({ id: 0 });
+              expect(res.body.data).to.not.have.property('_badFieldName');
+              return request(app).get('/admin/list/fields');
+            }).then(res => {
+              expect(res.statusCode).to.equal(200);
+              expect(res.body[0]).to.be.an('object').that.contains({ id: 0 });
+              expect(res.body[0]).to.not.have.property('_badFieldName');
+              done();
+            }).catch(done);
+        });
+    });
+
+
   });
 }
---
+++ test/models/All.js
@@ -0,0 +1,58 @@
+const allModels = require('../../lib/models/')();
+const _BaseModel = require('../../lib/models/_BaseModel');
+
+describe('Models (Standard)', () => {
+
+
+  Object.keys(allModels).forEach(modelKey => {
+    const model = allModels[modelKey];
+    const newModel = new model();
+    describe(`${modelKey}`, () => {
+      it('Should be have static properties tableName, modelName, and tableKeys', () => {
+        expect(newModel).to.contain.property('tableName').that.is.a('string');
+        expect(newModel).to.contain.property('modelName').that.is.a('string');
+        expect(newModel).to.contain.property('tableKeys').that.is.an('object');
+      });
+
+      it('Should be have baseModel static properties warnings and errors', () => {
+        expect(newModel).to.contain.property('warnings').that.is.an('array');
+        expect(newModel).to.contain.property('errors').that.is.an('array');
+      });
+
+      it('Should be have baseModel static properties cleanFields', () => {
+        expect(newModel).to.contain.property('cleanFields').that.is.an('object');
+      });
+
+      it('Should call base constructor with model data and instance data', () => {
+        const _BaseModelStub = sinon.sandbox.stub(_BaseModel, 'constructor');
+        const modelProxy = proxyquire(`../lib/models/${modelKey}`, { './_BaseModel' : _BaseModelStub });
+        const newModelProxy = new modelProxy({ id: 'test_id' });
+        expect(_BaseModelStub).to.be.called.once;
+        expect(_BaseModelStub.args[0]).to.have.length.gt(1);
+        expect(_BaseModelStub.args[0][0]).to.contain.keys('tableName', 'modelName', 'tableKeys');
+        expect(_BaseModelStub.args[0][1]).to.contain({ id: 'test_id' });
+        _BaseModelStub.restore();
+      });
+
+      it('tableKeys should have an id field', () => {
+        expect(newModel).to.contain.property('tableKeys').that.is.an('object').that.contains.property('id');
+      });
+
+      const fKeys =  Object.keys(newModel.tableKeys).filter(tk => {
+        return newModel.tableKeys[tk].hasOwnProperty('relation') ? newModel.tableKeys[tk] : null;
+      });
+
+      fKeys.forEach(fk => {
+        it(`Foreign key "${fk}" should have a "Model" property pointing to linked model`, () => {
+          expect(newModel.tableKeys)
+            .to.contain.property(fk)
+            .that.is.an('object')
+            .that.contains.property('relation')
+            .that.is.an('object')
+            .that.contains.property('model');
+        });
+      });
+    });
+  });
+  // TODO: test required fields return error, bad fields get filtered with warnings, test validation
+});
