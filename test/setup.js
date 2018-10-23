9efdba66f6e914244d581c27dec583f8742f01ac
Vagrant Default User <vagrant@stretch.localdomain>
2018-10-23 02:47:34 +0000

    Update project dependencies

--- test/setup.js
+++ test/setup.js
@@ -4,7 +4,8 @@ const sinonChai = require('sinon-chai');
 const chaiAsPromised = require('chai-as-promised');
 const proxyquire = require('proxyquire');
 const request = require('supertest');
-const { fatal, error, warn } = require('debugger-256')('api:test');
+// const { fatal, error, warn } = require('debugger-256')('api:test');
+// const { fatal, error, warn } = require('../lib/utils/logger')('api:test');
 
 
 console.log("setting up tests");
@@ -19,7 +20,7 @@ global.should = chai.should();
 global.proxyquire = proxyquire;
 global.request = request;
 
-process.on('uncaughtException', error);
-process.on('unhandledRejection', error);
+process.on('uncaughtException', console.error);
+process.on('unhandledRejection', console.error);
 //
 
