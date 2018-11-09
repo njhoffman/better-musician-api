7588907d23186d822dad0b614403f16bbdcb6835
Nick Hoffman <njhoffman1982@gmail.com>
2018-11-09 01:14:46 -0600

    Update package.json dependencies, remove piping package

--- bin/server.js
+++ bin/server.js
@@ -1,12 +1,2 @@
-/* eslint-disable global-require */
-if (process.env.NODE_ENV !== 'production') {
-  if (!require('piping')({
-    hook: true,
-    ignore: /(\/\.|~$|\.json$)/i
-  })) {
-    return;
-  }
-}
-/* eslint-enable global-require */
 require('../lib/utils/server.babel'); // babel registration (runtime transpilation for node)
 require('../lib/server')();
